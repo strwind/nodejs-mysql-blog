78db23e82acecbc3ac07b7f8a87010bbda3c9747
Fei Yao <gzzyyff@gmail.com>
2013-05-19 22:55:29 +0800

    Update db.js
    去掉注释

--- models/db.js
+++ models/db.js
@@ -1,10 +1,4 @@
-/*
-var settings = require('../settings'),
-    Db = require('mongodb').Db,
-    Connection = require('mongodb').Connection,
-    Server = require('mongodb').Server;
-module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT, {}));
-*/
+
 var mysql = require('mysql'),
     settings = require('../settings');
 var connection = mysql.createConnection({
@@ -15,13 +9,3 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 module.exports = connection;
-
-/*
-connection.query('SELECT * FROM users', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows);
-});
-
-connection.end();
-*/
